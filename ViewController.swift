4ef6692814027da45c56054af2f7695ce51e8f0d
zhaoxiao666 <zx5833@Mac30.local>
2016-05-12 11:41:08 +0800

    5.12
    链接到数据库

--- ViewController.swift
+++ ViewController.swift
@@ -6,9 +6,58 @@
 //  Copyright © 2016年 zx5833. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
+        
+        var db:SQLiteDB!
+        
+        @IBOutlet var txtUname: UITextField!
+        @IBOutlet var txtMobile: UITextField!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            //获取数据库实例
+            db = SQLiteDB.sharedInstance()
+            //如果表还不存在则创建表（其中uid为自增主键）
+            db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+            //如果有数据则加载
+            initUser()
+        }
+        
+        //点击保存
+        @IBAction func saveClicked(sender: AnyObject) {
+            saveUser()
+        }
+        
+        //从SQLite加载数据
+        func initUser() {
+            let data = db.query("select * from t_user")
+            if data.count > 0 {
+                //获取最后一行数据显示
+                let user = data[data.count - 1]
+                txtUname.text = user["uname"] as? String
+                txtMobile.text = user["mobile"] as? String
+            }
+        }
+        
+        //保存数据到SQLite
+        func saveUser() {
+            let uname = self.txtUname.text!
+            let mobile = self.txtMobile.text!
+            //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+            let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+            print("sql: \(sql)")
+            //通过封装的方法执行sql
+            let result = db.execute(sql)
+            print(result)
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
     @IBOutlet weak var defen: UILabel!
     var x:Double=0.0;
     var y:Double=0.0;
@@ -110,5 +159,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var sanfen2: UILabel!
     
     @IBOutlet weak var ershisi: UILabel!
+    
 
 }
